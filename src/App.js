cbfffb2c36ec2a706be8ad6c8e390b0ec98e5a9e
tcho <tcho@cca.edu>
2020-10-15 21:28:03 -0700

    creates a Jumbotron component + container that will display information for jumbotron -> now working on styled components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import jumboData from './fixtures/jumbo';
+import Jumbotron from './components/jumbotron';
+
 
 export default function App() {
   return (
-    <p>Hello</p>
+    <Jumbotron.Container>
+      {jumboData.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          <p>{item.title}</p>
+          <p>{item.subTitle}</p>
+          <p>{item.image}</p>
+          <p>{item.alt}</p>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
   );
 }
 
