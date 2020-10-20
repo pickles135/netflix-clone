48b0ebc0ab52966625b715b93aaed8a04a946470
tcho <teresacho135@gmail.com>
2020-10-19 23:23:00 -0700

    fixes type in minmax and adds some changes to responsiveness to grid and font-sizing

--- src/components/footer/styles/footer.js
+++ src/components/footer/styles/footer.js
@@ -21,7 +21,7 @@ export const Column = styled.div`
 
 export const Row = styled.div`
     display: grid;
-    grid-template-columns: repeat(auto-fill, min-max(230px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(210px, 1fr));
     grid-gap: 15px;
 
     @media (max-width) {
@@ -32,18 +32,18 @@ export const Row = styled.div`
 export const Link = styled.a`
     color: #757575;
     margin-bottom: 20px;
-    font-size: 13px;
+    font-size: 14px;
     text-decoration: none;
 `;
 
 export const Title = styled.p`
-    font-size: 16px;
+    font-size: 25px;
     color: #757575;
     margin-bottom: 40px;
 `;
 
 export const Text = styled.p`
-    font-size: 13px;
+    font-size: 17px;
     color: #757575;
     margin-bottom: 40px
 `;
