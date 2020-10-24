58bef44f2495bddcd3725643b538e6d990fa4d9f
tcho <teresacho135@gmail.com>
2020-10-23 17:53:39 -0700

    creates accordion component

---
+++ src/components/accordion/styles/accordion.js
@@ -0,0 +1,9 @@
+import React, {useState} from 'react';
+
+export default function Accordion ({ children, ...restProps}) {
+    return (
+        <Container {...restProps}>
+            <Inner>{children}</Inner>
+        </Container>
+    )
+}
