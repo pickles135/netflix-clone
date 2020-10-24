f35eba2a8b549ac3be7c0bd3aa2f00106ff85671
tcho <teresacho135@gmail.com>
2020-10-23 18:01:47 -0700

    creates subcomponents for Accordion, will create state for Item

--- src/components/accordion/styles/accordion.js
+++ src/components/accordion/styles/accordion.js
@@ -6,4 +6,17 @@ export default function Accordion ({ children, ...restProps}) {
             <Inner>{children}</Inner>
         </Container>
     )
-}
+};
+
+Accordion.Title = function AccordionTitle({ children, ...restProps}) {
+    return <Title {...restProps}>{children}</Title>
+};
+
+Accordion.Frame = function AccordionFrame({ children, ...restProps}) {
+    return <Frame {...restProps}>{children}</Frame>
+};
+
+Accordion.Item = function AccordionItem({ children, ...restProps}) {
+    return <Item {...restProps}>{children}</Item>
+};
+
