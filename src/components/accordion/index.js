be62a7ccced4ff83b868ba703c1a8f8351f7b35b
tcho <teresacho135@gmail.com>
2020-10-27 18:19:16 -0700

    creates a render for the header for open and close for toggleShow

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -37,6 +37,11 @@ Accordion.Header = function AccordionHeader({ children, ...restProps}) {
 
     return (<Header onClick={() => setToggleShow((toggleShow) => !toggleShow)} {...restProps}>
         {children}
+        {toggleShow ? (
+            <img src="/images/icons/close-slim.png" alt="Close" />
+        ) : (
+            <img src="/images/icons/add.png" alt="Open" />
+        )}
     </Header>
     );
 };
