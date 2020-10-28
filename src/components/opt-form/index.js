e942de90f1f679d63dd02dda3665742440e28a37
tcho <teresacho135@gmail.com>
2020-10-27 22:03:36 -0700

    creates the components for the OptForm

--- src/components/opt-form/index.js
+++ src/components/opt-form/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Input, Button, Text } from './styles/opt-form';
+import { Container, Input, Button, Text, Break } from './styles/opt-form';
 
 export default function OptForm({ children, ...restProps}) {
     return <Container {...restProps}>{children}</Container>
@@ -8,7 +8,6 @@ export default function OptForm({ children, ...restProps}) {
 OptForm.Input = function OptFormInput({ ...restProps }) {
     return <Input {...restProps} />
 };
-
 
 OptForm.Button = function OptFormButton({ children, ...restProps}) {
     return (
@@ -20,4 +19,8 @@ OptForm.Button = function OptFormButton({ children, ...restProps}) {
 
 OptForm.Text = function OptFormText({ children, ...restProps}) {
     return <Text {...restProps}>{children}</Text>
+};
+
+OptForm.Break = function OptFormBreak({ ...restProps}) {
+    return <Break {...restProps}></Break>
 };
