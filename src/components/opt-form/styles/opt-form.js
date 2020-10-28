fe0f5dbc13e250c558557ff2c503aaebc42e0272
tcho <teresacho135@gmail.com>
2020-10-27 18:49:15 -0700

    creates some basic boilerplate for the opt-form

---
+++ src/components/opt-form/styles/opt-form.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/macro';
+
+export const Container = styled.div``;
+
+export const Input = styled.div``;
+
+export const Button = styled.button``;
+
+export const Text = styled.p``;
+
