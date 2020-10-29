bb27751a546817ec19986bab567ab30f165fdecc
tcho <teresacho135@gmail.com>
2020-10-28 18:43:26 -0700

    imports react-router and built home path

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { JumbotronContainer } from './containers/jumbotron';
 import { FooterContainer } from './containers/footer';
 import { FaqsContainer } from './containers/faqs';
+import * as ROUTES from './constants/routes';
 
 
 export default function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </>
+    <Router>
+      <Route exact path={ROUTES.HOME}>
+        <JumbotronContainer />
+        <FaqsContainer />
+        <FooterContainer />
+      </Route>
+    </Router>
   );
 }
 
