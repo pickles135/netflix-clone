4b5e00d2260058fa9233c55b5972727cf0e239ec
tcho <teresacho135@gmail.com>
2020-11-08 21:51:13 -0800

    adds the resuable optform from earlier and creates feature

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HeaderContainer } from '../containers/header';
+import { Feature, OptForm } from '../components';
 import { JumbotronContainer } from '../containers/jumbotron';
 import { FooterContainer } from '../containers/footer';
 import { FaqsContainer } from '../containers/faqs';
@@ -8,10 +9,27 @@ export default function Home() {
     return (
         <>
             <HeaderContainer>
-                <JumbotronContainer />
-                <FaqsContainer />
-                <FooterContainer />
+                <Feature>
+                    <Feature.Title>
+                        Unlimited films, TV programs and more.
+                    </Feature.Title>
+                    <Feature.Subtitle>
+                        Watch anywhere. Cancel at any time.
+                    </Feature.Subtitle>
+                </Feature>
+                <OptForm>
+                    <OptForm.Input placeholder="Email Address" />
+                    <OptForm.Button>Get Started</OptForm.Button>
+                    <OptForm.Text>
+                        Ready to watch? Enter your email to create or restart your membership
+                    </OptForm.Text>
+                    <OptForm.Break />
+                </OptForm>
             </HeaderContainer>
+
+            <JumbotronContainer />
+            <FaqsContainer />
+            <FooterContainer />
         </>
     )
 };
