a32c59aef18e5ee9984bd783ef4b10c04886a3e8
tcho <teresacho135@gmail.com>
2020-11-09 00:04:13 -0800

    refractors the imports and also fixes the typo in Feature components

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,35 +1,29 @@
 import React from 'react';
-import { HeaderContainer } from '../containers/header';
 import { Feature, OptForm } from '../components';
+import { HeaderContainer } from '../containers/header';
 import { JumbotronContainer } from '../containers/jumbotron';
-import { FooterContainer } from '../containers/footer';
 import { FaqsContainer } from '../containers/faqs';
+import { FooterContainer } from '../containers/footer';
 
 export default function Home() {
     return (
         <>
             <HeaderContainer>
                 <Feature>
-                    <Feature.Title>
-                        Unlimited films, TV programs and more.
-                    </Feature.Title>
-                    <Feature.Subtitle>
-                        Watch anywhere. Cancel at any time.
-                    </Feature.Subtitle>
+                    <Feature.Title>Unlimited films, TV programs and more.</Feature.Title>
+                    <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
+                    <OptForm>
+                        <OptForm.Input placeholder="Email Address" />
+                        <OptForm.Button>Get Started</OptForm.Button>
+                        <OptForm.Break />
+                        <OptForm.Text>Ready to watch? Enter your email to create or restart your membership</OptForm.Text>
+                    </OptForm>
                 </Feature>
-                <OptForm>
-                    <OptForm.Input placeholder="Email Address" />
-                    <OptForm.Button>Get Started</OptForm.Button>
-                    <OptForm.Text>
-                        Ready to watch? Enter your email to create or restart your membership
-                    </OptForm.Text>
-                    <OptForm.Break />
-                </OptForm>
             </HeaderContainer>
 
             <JumbotronContainer />
             <FaqsContainer />
             <FooterContainer />
         </>
-    )
+    );
 };
