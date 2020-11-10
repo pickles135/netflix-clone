6a0f5d9c6c8c7d1da063453694b22cf4eb1eb524
tcho <teresacho135@gmail.com>
2020-11-09 21:00:56 -0800

    creates our firebase config

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -5,7 +5,15 @@ import 'firebase/auth';
 //seed db
 
 //config here. 
-const config = {}; //temp
+const config = {
+    apiKey: "AIzaSyAjVy0CtnJOIGnqdQw47e34IIbotU5GnoU",
+    authDomain: "netflix-clone-19921.firebaseapp.com",
+    databaseURL: "https://netflix-clone-19921.firebaseio.com",
+    projectId: "netflix-clone-19921",
+    storageBucket: "netflix-clone-19921.appspot.com",
+    messagingSenderId: "853420734523",
+    appId: "1:853420734523:web:8491ec469d77e668f2041e"
+};
 
 const firebase = Firebase.initializeApp(config);
 
